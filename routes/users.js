53ab0949108ead0a1f5ca2a6ea7fe22abcff9da3
arbazkhan2508 <ak1933929@gmail.com>
2023-11-16 00:38:53 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,118 @@
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+mongoose
+  .connect("mongodb://0.0.0.0/insta")
+  .then(function (connected) {
+    console.log("connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+let userSchema = mongoose.Schema(
+  {
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    Uposts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    Bookmark: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    username: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    userProfileimg: String,
+    dob: {
+      type: Date,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    token: {
+      type: String,
+      default: "",
+    },
+    expiringTime: String,
+    Caption: String,
+    chats: {
+      type: Map,
+      of: Object,
+    },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    stories: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "story",
+      },
+    ],
+    archive: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "story",
+      },
+    ],
+    reels: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    Comment: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comment",
+      },
+    ],
+    password: String,
+  },
+  { timestamps: true }
+);
+userSchema.plugin(plm, { usernameField: "email" });
+userSchema.plugin(findOrCreate);
+module.exports = mongoose.model("user", userSchema);
